e954e464fdedf67f027f957dbbb8c312dd5dca7f
HamiltonStefani <88253849+HamiltonStefani@users.noreply.github.com>
2023-01-12 23:42:25 -0300

    Add files via upload

---
+++ whileexercicio02/Main.java
@@ -0,0 +1,34 @@
+package whileexercicio02;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite os números");
+		
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		
+		while(x != 0 && y != 0) {
+			if(x > 0 && y > 0) {
+				System.out.println("PRIMEIRO");
+			}else if(x < 0 && y > 0) {
+				System.out.println("SEGUNDO");
+			}else if(x < 0 && y < 0) {
+				System.out.println("TERCEIRO");
+			}else {
+				System.out.println("QUARTO");
+			}
+			x = sc.nextInt();
+			y = sc.nextInt();
+		}
+		
+		sc.close();
+
+	}
+
+}
